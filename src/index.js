2714050bf268888683ace75b38aada8a3efbd2ac
Or Keren <or.keren@checkmarx.com>
2020-01-27 10:09:05 +0200

    hello world

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+//import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { render } from '@testing-library/react';
+
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+})
+
+
+const EXCHANGE_RATES = gql`
+    {
+        rates(currency: "USD") {
+            currency
+            rate
+        }
+    }
+`;
+
+function ExchangeRates() {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Error :(</p>;
+
+    return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+            <p>
+                {currency}: {rate}
+            </p>
+        </div>
+    ));
+}
+
+
+const App = () => (
+    <ApolloProvider client={client}>
+        <div>
+            <h2>My first Apollo App</h2>
+            <ExchangeRates />
+        </div>
+    </ApolloProvider>
+);
+
+render(<App />, document.getElementById('root'));
+
+
+// client.query({
+//     query: gql`
+//         {
+//             rates(currency: "USD") {
+//                 currency
+//             }
+//         }
+//     `
+// }).then(result => console.log(result));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
