aad0b944bdfa96b77c3471a4f6075158c6c85c53
Or Keren <or.keren@checkmarx.com>
2020-01-28 14:33:29 +0200

    fetches from graphql and REST

--- src/components/WishlistsContainer.jsx
+++ src/components/WishlistsContainer.jsx
@@ -47,29 +47,74 @@ function WishlistEntries({wishlistID}) {
 
     return (
         data.wishlistEntryByWishlistID.map(entry => (
-            <p>
+            <div>
                 <div>{ entry.id } </div>
                 <div> { entry.reservableUUID } </div>
                 <div> { entry.createdBy }  </div>
-            </p>
+            </div>
         )));
 }
 
 class WishlistsContainer extends React.Component{
-    state = { selectedWishlist: null}
+    state = { selectedWishlist: null, selectedWishlist2: null}
+
+    componentDidMount() {
+        fetch('http://localhost:4000/api/v1/wishlists/')
+            .then(response => response.json())
+            .then(data => this.setState({ wishlists: data.wishlists }));
+    };
 
     onWishlistSelected = ({ target }) => {
-        console.log('changed wishlist' + target.value);
         this.setState(() => ({ selectedWishlist: target.value }));
     };
 
+    onWishlistSelected2 = ({ target }) => {
+        fetch(`http://localhost:4000/api/v1/wishlist-entries?wishlistID=${target.value}`)
+            .then(response => response.json())
+            .then(data => this.setState({ entries: data.entries }));
+    };
+
+
+    renderWishlists = () => {
+        return(
+                <select name="wishlist" onChange={this.onWishlistSelected2}>
+                    {this.state.wishlists.map(wishlist => (
+                        <option key={wishlist.id} value={parseInt(wishlist.id, 10)}>
+                            {wishlist.opportunitySFID}
+                        </option>
+                    ))}  
+                </select>
+        );
+    }
+
+    renderEntries= () => {
+        const entries = this.state.entries;
+        return (
+            entries.map(entry => (
+                <div>
+                    <div>{ entry.id } </div>
+                    <div> { entry.reservableUUID } </div>
+                    <div> { entry.createdBy }  </div>
+                </div>
+            )));
+    }
+
+
     render() {
         return (
-        <div>
-            <WishlistsList onWishlistSelected={this.onWishlistSelected} />
+            <div>
+                <div>
+                    <WishlistsList onWishlistSelected={this.onWishlistSelected} />
 
-            { this.state.selectedWishlist &&  <WishlistEntries wishlistID={this.state.selectedWishlist}/>}
-        </div>
+                    { this.state.selectedWishlist &&  <WishlistEntries wishlistID={this.state.selectedWishlist}/>}
+                </div>
+                <div>
+                    { this.state.wishlists && this.renderWishlists() }
+                </div>
+                <div>
+                    {this.state.entries && this.renderEntries() }
+                </div>
+            </div>
         );
     }
 }
